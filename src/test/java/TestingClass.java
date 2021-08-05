e8d5bfe6e0d30aa431ae4f2c69e8ef69a3a6476c
jp10 <123steva123@gmail.com>
2021-08-05 17:24:11 +0200

    First commit

---
+++ src/test/java/TestingClass.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class TestingClass {
+
+
+   @Test(groups = "FirstTest")
+    public void simpleTest(){
+        System.out.println("This is test.");
+    }
+
+}
